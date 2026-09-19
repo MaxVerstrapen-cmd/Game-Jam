91fa541ebe514af9b65627908bebf28e930693f8
Matt <mattyeats105080@gmail.com>
2026-09-19 08:55:07 -0600

    made player animations fixed main menu using template assests till we get assests made

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); // A = -1, D = +1
+
+        anim.SetBool("isRunning", x != 0);
+
+        // Flip to face the direction he's moving
+        if (x != 0)
+        {
+            float size = Mathf.Abs(transform.localScale.x);
+            transform.localScale = new Vector3(x < 0 ? -size : size, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
